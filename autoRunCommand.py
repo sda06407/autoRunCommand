474acbe0121bd853a6f14f01a9f081cf791be023
sda06407 <sda06407@gmail.com>
2020-05-10 13:20:47 +0800

    init

---
+++ autoRunCommand.py
@@ -0,0 +1,17 @@
+import os, sys
+
+allTarget = sys.argv[2]
+targetList = open(allTarget, "r")
+resultPath = sys.argv[3]
+Command = sys.argv[1]
+
+if not os.path.isdir(sys.argv[3]):
+    os.mkdir(sys.argv[3])
+
+for oneSite in targetList.readlines():
+    scanCommand = Command + " " + oneSite[:-1]
+    scanResult = os.popen("%s" %scanCommand).read()
+    resultName = oneSite.replace("/","").replace(":","").replace("\n","").replace("\r", "") + ".txt"
+    makeResult = open(os.path.abspath(resultPath) + "//" + resultName, "w")
+    makeResult.write(scanResult)
+    makeResult.close()
